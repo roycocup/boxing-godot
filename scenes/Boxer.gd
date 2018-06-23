f2b2b2a69579870103e30d4f36a71f5820b0ea01
Rodrigo Dias <rodrigo.pitta@gmail.com>
2018-06-23 18:08:05 +0100

    animations

---
+++ scenes/Boxer.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+export var speed = 1
+enum status {IDLE, RIGHT, LEFT, HIT}
+var cur_status
+
+
+func change_status(finished_animation):
+	if (cur_status != status.IDLE):
+		cur_status = status.IDLE
+
+func _ready():
+	cur_status = status.IDLE
+	$Player.connect("animation_finished", self, "change_status")
+	pass
+
+func _process(delta):
+	if cur_status == status.IDLE:
+		$Player.play("Idle")
+	
+	if Input.is_key_pressed(KEY_Z):
+		cur_status = status.LEFT
+		$Player.play("Left")
+		
---
+++ scenes/World.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+
+func _ready():
+	set_process(true)
+
+func _process(delta):
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().quit()
+
+
