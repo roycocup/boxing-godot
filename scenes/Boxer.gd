d7bc5cf3ad198ee520e22ab204cb2cd5bfd48898
Rodrigo Dias <rodrigo.pitta@gmail.com>
2018-06-23 20:45:39 +0100

    movement and faster punches

--- scenes/Boxer.gd
+++ scenes/Boxer.gd
@@ -1,6 +1,6 @@
 extends KinematicBody2D
 
-export var speed = 1
+export var velocity = 1
 
 enum status {IDLE, RIGHT, LEFT, HIT}
 var cur_status
@@ -16,15 +16,29 @@ func _process(delta):
 
 
 func listen_to_keys():
+	movement()
+	shots()
+
+func shots():
 	if Input.is_key_pressed(KEY_Z):
 		cur_status = status.LEFT
-		
 	if Input.is_key_pressed(KEY_X):
-		cur_status = status.RIGHT
-		
+		cur_status = status.RIGHT		
 	if Input.is_key_pressed(KEY_0):
 		cur_status = status.HIT
 
+func movement():
+	if Input.is_action_pressed("ui_right"):
+		position.x += (speed + position.x) * get_process_delta_time()
+	if Input.is_action_pressed("ui_left"):
+		position.x -= (speed + position.x) * get_process_delta_time()
+	if Input.is_action_pressed("ui_up"):
+		position.y -= (speed + position.y) * get_process_delta_time()
+	if Input.is_action_pressed("ui_down"):
+		position.y += (speed + position.y) * get_process_delta_time()
+	 move_and_slide( velocity, Vector2(0, -1) )
+		
+		
 func animate():
 	match cur_status:
 		status.HIT:
