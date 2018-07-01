51b295dc0509b79aa87b0972ff098078098e58ea
Rodrigo Dias <rodrigo.pitta@gmail.com>
2018-07-01 23:16:21 +0100

    punches

--- scenes/Enemy.gd
+++ scenes/Enemy.gd
@@ -3,7 +3,12 @@ extends "res://scenes/Character.gd"
 func _physics_process(delta):
 	if (cur_status == status.IDLE):
 		animate()
-		
+	
+
 
 func _on_Hitbox_area_shape_entered(area_id, area, area_shape, self_shape):
- 	$Player.play("Hit")
+	var back = -1
+	if rotation > 0:
+		back = 1 
+	position.x = position.x + (5 * back) 
+	$Player.play("Hit")
