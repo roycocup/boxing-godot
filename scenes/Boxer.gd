f4f29e363765eeb95aced4930c003fd41828242d
Rodrigo Dias <rodrigo.pitta@gmail.com>
2018-10-25 14:46:16 +0100

    finishing hitboxes and hurtboxes

--- scenes/Boxer.gd
+++ scenes/Boxer.gd
@@ -38,9 +38,13 @@ func animate():
 				$Player.play("Idle")
 		
 
+func move_back(amountpx):
+	if rotation > 0:
+		amountpx = amountpx * 1 # invert signal
+	position.x = position.x + amountpx
 
-
-func _on_Hitbox_area_shape_entered(area_id, area, area_shape, self_shape):
+func _on_face_hit(area_id, area, area_shape, self_shape):
+	move_back(25)
 	cur_status = status.HIT
 	$Player.play("Hit")
 	cur_status = status.IDLE
--- scenes/Enemy.gd
+++ scenes/Enemy.gd
@@ -5,20 +5,11 @@ func _physics_process(delta):
 		animate()
 	
 
-func hit(area_id, area, area_shape, self_shape):
-	pass	
-
-
-func _on_Area2D_body_shape_entered(body_id, body, body_shape, area_shape):
-	print('body')
-	pass # replace with function body
-
-
 func move_back(amountpx):
 	if rotation > 0:
 		amountpx = amountpx * 1 # invert signal
 	position.x = position.x + amountpx 
 
-func _on_Area2D_area_shape_entered(area_id, area, area_shape, self_shape):
+func _on_face_hit(area_id, area, area_shape, self_shape):
 	move_back(25)
 	$Player.play("Hit")
