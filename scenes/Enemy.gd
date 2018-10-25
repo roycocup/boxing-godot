99643948b3c5c03f5e8797871575e02d89faba62
Rodrigo Dias <rodrigo.pitta@gmail.com>
2018-10-25 11:58:41 +0100

    collisions starting to work

--- scenes/Enemy.gd
+++ scenes/Enemy.gd
@@ -5,9 +5,20 @@ func _physics_process(delta):
 		animate()
 	
 
-func _on_Hitbox_area_shape_entered(area_id, area, area_shape, self_shape):
-	var back = -1
+func hit(area_id, area, area_shape, self_shape):
+	pass	
+
+
+func _on_Area2D_body_shape_entered(body_id, body, body_shape, area_shape):
+	print('body')
+	pass # replace with function body
+
+
+func move_back(amountpx):
 	if rotation > 0:
-		back = 1 
-	position.x = position.x + (5 * back) 
+		amountpx = amountpx * 1 # invert signal
+	position.x = position.x + amountpx 
+
+func _on_Area2D_area_shape_entered(area_id, area, area_shape, self_shape):
+	move_back(25)
 	$Player.play("Hit")
