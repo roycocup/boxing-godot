23612668308b9a8447ba493fbc7d0ed73f11786d
Rodrigo Dias <rodrigo.pitta@gmail.com>
2018-11-24 18:03:13 +0000

    starting ai

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -1,19 +1,26 @@
 extends "res://scripts/Character.gd"
 
 var frame = 0
-
+onready var AI = preload("res://scripts/AI.gd").new()
 
 func _physics_process(delta):
 	frame_count()
 	if (cur_status == status.IDLE):
 		animate()
 	if (frame % 30 == 1):
-		$Player.play("Left")
+		print(AI)
+		left_punch()
 
 func frame_count():
 	frame = frame + 1
 	if (frame >= 60):
 		frame = 0
+
+func left_punch():
+	$Player.play("Left")
+
+func right_punch():
+	$Player.play("Right")
 	
 
 func _on_face_hit(area_id, area, area_shape, self_shape):
