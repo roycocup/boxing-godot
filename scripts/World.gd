d8738c6d6aadaf1a73e1096afe13063e3e25418c
Rodrigo Dias <rodrigo.pitta@gmail.com>
2018-12-14 18:42:08 +0000

    more

--- scripts/World.gd
+++ scripts/World.gd
@@ -4,21 +4,30 @@ onready var Cache = preload("res://scripts/Cache.gd").new()
 onready var Players = {'p1':$Canvas/Boxer, 'p2':$Canvas/AI}
 var Score = {'p1':0, 'p2':0, 'is_dirty':false}
 var Time = 90 # seconds
+onready var UI = {
+	'p1_score':$UI/HBoxContainer/MarginContainer/p1_Score,
+	'p2_score':$UI/HBoxContainer/p2_Score,
+	'time':$UI/HBoxContainer/MarginContainer2/time,
+}
 
 func _ready():
 	set_process(true)
 	Cache.cache_filename = 'res://scripts/World.cache'
-	$Bell.play()
-	$Music.play()
+	#$Bell.play()
 	
 
 func _process(delta):
 	quit_by_esc()
 	save_score()
+	update_ui()
 		
 func quit_by_esc():
 	if Input.is_action_pressed("exit"):
 		get_tree().quit()
+
+func update_ui():
+	UI['p1_score'].text = str(Score['p1'])
+	UI['p2_score'].text = str(Score['p2'])
 	
 func save_score():
 	if (Score['is_dirty']):
@@ -33,7 +42,6 @@ func update_score(player_data):
 		Score['p1'] = Score['p1'] + 1
 	else:
 		return
-	print(Score)
 	Score['is_dirty'] = true
 
 
