6234c3f1277b01131e787dd4bb4aa9d08c946ccd
Rodrigo Dias <rodrigo.pitta@gmail.com>
2018-12-14 23:09:38 +0000

    more

--- scripts/World.gd
+++ scripts/World.gd
@@ -1,14 +1,18 @@
 extends Node2D
 
-onready var Cache = preload("res://scripts/Cache.gd").new()
-onready var Players = {'p1':$Canvas/Boxer, 'p2':$Canvas/AI}
 var Score = {'p1':0, 'p2':0, 'is_dirty':false}
 var Level = 1
+var game_over = false
+
+onready var Cache = preload("res://scripts/Cache.gd").new()
+onready var Players = {'p1':$Canvas/Boxer, 'p2':$Canvas/AI}
 onready var UI = {
 	'p1_score':$UI/HBoxContainer/MarginContainer/p1_Score,
 	'p2_score':$UI/HBoxContainer/p2_Score,
 	'time':$UI/HBoxContainer/MarginContainer2/time,
 }
+
+
 
 func _ready():
 	set_process(true)
@@ -19,17 +23,20 @@ func _process(delta):
 	quit_by_esc()
 	save_score()
 	update_ui()
-		
+
+
+
 func quit_by_esc():
 	if Input.is_action_pressed("exit"):
 		get_tree().quit()
 
 func get_time_left_str():
-	var secs = round($Timer.time_left)
-	var minutes = round(secs/60)
+	var secs = fmod(round($Timer.time_left), 60)
+	var minutes = floor($Timer.time_left/60)
 	if (minutes <= 0): 
 		minutes = 0
-	secs = str(secs - (minutes * 60))
+	secs = str(secs)
+	minutes = str(minutes)
 	if len(secs) < 2:
 		secs = "0" + secs
 	var time = str(minutes) + ":" + str(secs)
